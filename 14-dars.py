7e101801468ea9b8c6c5ee8a7f03fe75d8b43851
Mur0dDev <msadullaevdev@gmail.com>
2024-07-09 22:36:41 +0500

    Created 14-dars
    Answers for all task of Lesson 14

---
+++ 14-dars.py
@@ -0,0 +1,58 @@
+"""
+
+Dasturlash asoslari
+
+#14-dars: DICTIONARY
+
+Muallif: Murodjon Sadullaev
+
+"""
+
+otam = {'ismi':'mavlutdin', 'tyil':1954,'viloyat':'samarqand'}
+tyil = otam['tyil']
+vil = otam['viloyat']
+print(f"Otamning ismi {otam['ismi'].title()}, {tyil}-yilda, {vil.title()} viloyatida tug'ilgan")
+
+taomlar = {
+    'ali':'osh',
+    'vali':'shashlik',
+    'hasan':"lag'mon",
+    'husan':"mastava",
+    'olim':"somsa"
+    }
+
+taom = taomlar['ali']
+print(f"Alining sevimli taomi {taom}")
+
+python_izohli_lugati = {
+    'integer':"Butun son",
+    'float':"O'nlik son",
+    'string':"Matn",
+    'list':"Ro'yxat",
+    'tuple':"O'zgarmas ro'yxat"}
+# print(python_izohli_lugati['tuple'])
+
+kalit = input("Kalit so'z kiriting:").lower()
+print(python_izohli_lugati.get(kalit,"Bunday so'z mavjud emas"))
+
+kalit = input("Kalit so'z kiriting:").lower()
+tarjima = python_izohli_lugati.get(kalit)
+if tarjima==None:
+    print("Bunday so'z mavjud emas")
+else:
+    print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi")
+    
+"""
+
+otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga shu inson haqida kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo). Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring :Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+
+Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom jufltigi bo'lsin. Kamida uch kishining sevimli taomini konsolga chiqaring: Alining sevimli taomi osh
+
+Python izohli lu'gati tuzing: Lug'atga shu kunga qadar o'rgangan 10 ta so'z (atamani) kiriting (masalan integer, float, string, if, else va hokazo) va har birining qisqacha tarjimasini yozing.
+
+Foydalanuvchidan biror so'z kiritishni so'rang va so'zning tarjimasini yuqoridagi lug'atdan chiqarib bering. Agar so'z lu'gatda mavjud bo'lmasa, "Bunda so'z mavjud emas" degan xabarni chiqaring.
+
+
+Yuqoridagi vazifani if-else yordamida qiling va natijani ham foydalanuvchiga tushunarli ko'rinishda chiqaring.
+
+"""
